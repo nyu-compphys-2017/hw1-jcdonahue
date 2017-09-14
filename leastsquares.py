20a29b3860d9e1f6f98f7a461cae2776d225bd9a
jcdonahue <jackdnhue@gmail.com>
2017-09-14 15:23:08 -0400

    commented the code for easy understanding of the function

--- leastsquares.py
+++ leastsquares.py
@@ -1,27 +1,30 @@
 from __future__ import division
 import numpy as np
-from pylab import scatter,imshow,show,plot
+from pylab import scatter,imshow,show,plot,xlabel,ylabel,xlim,ylim,suptitle
 
-a = np.loadtxt("millikan.txt",float)
-x = a[:,0]*10**-14
-y = a[:,1]
-N = len(x)
+a = np.loadtxt("millikan.txt",float)                                    # imports data from the txt files
+x = a[:,0]*10**-14                                                      # imports the frequency  values into an array named x, normalized to get rid of 10^-14 offset
+y = a[:,1]                                                              # import voltages to y
+N = len(x)                                                              # gets the length to be used for later
 
-Ex = np.sum(x)/N
+Ex = np.sum(x)/N                                                        # calculates values needed for getting best fit parameters 
 Ey = np.sum(y)/N
 Exx = np.sum(x*x)/N
 Exy = np.sum(x*y)/N
 
-m = (Exy-Ex*Ey)/(Exx-Ex*Ex)
+m = (Exy-Ex*Ey)/(Exx-Ex*Ex)                                             # calculates best fit parameters
 c = (Exx*Ey-Ex*Exy)/(Exx-Ex*Ex)
 
-pred = [m*i+c for i in x]
-h = m*16.02
-hquote = 6.262
-pd = 100*(h-hquote)/hquote
+pred = [m*i+c for i in x]                                               # predicts values for each voltage based on best fit, was important in code testing
+h = m*16.02                                                             # calculates h from the slope of the best fit
+hquote = 6.262                                                          # quoted value of planck's constant
+pd = 100*(h-hquote)/hquote                                              # calculates and displays the percent error of planck's constant
 print "%(h)f 10^-34 J*s" % {"h":h}
 print "This is %(pd)f percent different from" % {"pd": pd}
 print "current quoted values of Planck's constant"
-scatter(x,y)
+scatter(x,y)                                                            # displays graphs
 plot(x,pred)
+xlabel("Frequency (10^14 Hz)")
+ylabel("Stopping Voltage (V)")
+suptitle("Frequency v. Stopping Voltage from the Millikan Oil Drop experiment")
 show()
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,33 +1,39 @@
 from __future__ import division
-from pylab import imshow,show,hot,jet
+from pylab import imshow,show,hot,jet,xlabel,ylabel,suptitle
 import numpy as np
 import math
 
-N = int(raw_input("Grid Size:   "))       # gridsize
-maxiter = int(raw_input("Maximum Iterations:  "))         # maxiterations
+N = int(raw_input("Grid Size:   "))       # user sets gridsize
+maxiter = int(raw_input("Maximum Iterations:  "))         # user sets maxiterations
 
-def mandelbrot(maxiter, N):
-    a = np.zeros((N,N), dtype=int)
-    i = 0
+def mandelbrot(maxiter, N):                         # function to create mandelbrot set
+    a = np.zeros((N,N), dtype=int)                  # initialize NxN array with all zeros
+    i = 0                                           # initialize i and k at zero, they will be indexes that run over the array
     k = 0
-    step = 4/N
-    for i in range(0,N):
+    step = 4/N                                      # calculates the step size to break up the 4x4 sqaure of values we want to calculate the set for
+    for i in range(0,N):                            # these for loops run the mandelbrot test over each entry in the array
         for k in range(0,N):
-            iter  = 0
+            iter  = 0                               # initialize iterations and z at zero
             z = complex(0,0) 
-            c = complex(-2+i*step,2-k*step)
-            for  iter in range(1,maxiter):
-                if (abs(z) <= 2):
-                    z = z*z + c
-                    iter = iter + 1
+            c = complex(-2+i*step,2-k*step)         # set c, the variable in the iterated map. This will choose values in an NxN square superimposed on the 4x4 sqaure
+            for  iter in range(1,maxiter):          # have the for loop run up until the max iter set by the user
+                if (abs(z) <= 2):                   
+                    z = z*z + c                     # sqaures z and adds c until the absoulte value of z exceedes 2, then breaks the for loop.
+                    iter = iter + 1                 # this preserves the number of iterations it took to exceed 2
                 else:
                     break
-            a[k,i] = math.log(iter,1.15)
-    return a
+            a[k,i] = math.log(iter,1.15)            # this then sets the value of the array entry to the log of the number of iterations. 1.15 was chosen to increase the color variations in interesting regions
+    return a                                        # outputs the populated array
 
-data = mandelbrot(maxiter, N)
+data = mandelbrot(maxiter, N)                      
 imshow(data)
-jet()
+jet()                                               # runs mandelbrot with the user defined values and displays it in Jet coloring
+
+imshow(data).axes.get_xaxis().set_ticks([])
+imshow(data).axes.get_yaxis().set_ticks([])
+suptitle("Mandelbrot Set")
+xlabel("Real")
+ylabel("Imaginary")
 show()
 
 
